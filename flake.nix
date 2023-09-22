fd3aa57fc3faaa6fad415befff7792e1c5694307
Egor Lappo <elappo@stanford.edu>
2023-09-22 16:02:53 -0700

    working on darwin

--- flake.nix
+++ flake.nix
@@ -14,15 +14,42 @@
       let
         pkgs = import nixpkgs { inherit system; };
 
+        inherit (pkgs.stdenv) isAarch32 isAarch64 isDarwin;
+
         eig-drv = pkgs.stdenv.mkDerivation
           {
             name = "eigensoft";
             version = "8";
-            src = ./src;
+            src = ./.;
 
             NIX_CFLAGS_COMPILE = "-fcommon";
 
+            buildInputs = [
+              pkgs.gsl
+              pkgs.openblas
+            ] ++ (
+              if isAarch64 && isDarwin then
+                with pkgs.darwin.apple_sdk_11_0.frameworks; [
+                  Accelerate
+                  MetalKit
+                ]
+              else if isAarch32 && isDarwin then
+                with pkgs.darwin.apple_sdk.frameworks; [
+                  Accelerate
+                  CoreGraphics
+                  CoreVideo
+                ]
+              else if isDarwin then
+                with pkgs.darwin.apple_sdk.frameworks; [
+                  Accelerate
+                  CoreGraphics
+                  CoreVideo
+                ]
+              else [ ]
+            );
+
             buildPhase = ''
+              cd src
               make install
             '';
 
